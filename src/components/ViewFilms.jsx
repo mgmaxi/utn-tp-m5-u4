e7905e067c695f92186d09e9ea27fa41a666646c
mgmaxi <maximiliano.ghirardi@gmail.com>
2021-07-20 00:58:28 -0300

    Add try catch

--- src/components/ViewFilms.jsx
+++ src/components/ViewFilms.jsx
@@ -3,28 +3,33 @@ import axios from 'axios';
 import './viewFilms.css';
 
 export default function ViewFilms() {
-    const [data, setData] = React.useState([])
-    useEffect(() => {
-        async function fetchData(){
-        const response = await axios.get('http://localhost:3000/test')
-        if (response.status === 200) {
-        setData(response.data)
-        }}
+
+	const [data, setData] = React.useState([]);
+	
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+                const response = await axios.get('http://localhost:3000/test');
+                if (response.status === 200) {
+                    setData(response.data)
+                } 
+            } catch(error) {
+                console.log(error)
+			}
+        }
         fetchData()
-    }, [])
-
-    const array = data.map((list)=>{
-        return (
-            <div className="card" key={list.pelicula_id}>
-                <div className="cardTitle">{list.pelicula_titulo}</div>
-                <div className="cardSynopsis">{list.pelicula_sinopsis}</div>
-            </div>
-        )
-    })
-
-    return (
-        <div className="container">
-            {array}
-        </div>
-    )
-}
+	}, []);
+		
+	const array = data.map((list)=>{
+		return (
+			<div className="card" key={list.pelicula_id}>
+				<div className="cardTitle">{list.pelicula_titulo}</div>
+				<div className="cardSynopsis">{list.pelicula_sinopsis}</div>
+			</div>
+		);
+	});
+    
+	return (
+		<div className="container">{array}</div>
+	);
+};
