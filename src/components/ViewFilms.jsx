fd01c393f765b201d9f9204e173c47af48423e80
mgmaxi <79719065+mgmaxi@users.noreply.github.com>
2021-07-20 01:14:39 -0300

    Indent code

--- src/components/ViewFilms.jsx
+++ src/components/ViewFilms.jsx
@@ -4,32 +4,32 @@ import './viewFilms.css';
 
 export default function ViewFilms() {
 
-	const [data, setData] = React.useState([]);
-	
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-                const response = await axios.get('http://localhost:3000/test');
-                if (response.status === 200) {
-                    setData(response.data)
-                } 
-            } catch(error) {
-                console.log(error)
-			}
+  const [data, setData] = React.useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/test');
+        if (response.status === 200) {
+          setData(response.data)
         }
-        fetchData()
-	}, []);
-		
-	const array = data.map((list)=>{
-		return (
-			<div className="card" key={list.pelicula_id}>
-				<div className="cardTitle">{list.pelicula_titulo}</div>
-				<div className="cardSynopsis">{list.pelicula_sinopsis}</div>
-			</div>
-		);
-	});
-    
-	return (
-		<div className="container">{array}</div>
-	);
+      } catch(error) {
+          console.log(error)
+      }
+    }
+    fetchData()
+  }, []);
+
+  const array = data.map((list)=>{
+    return (
+      <div className="card" key={list.pelicula_id}>
+        <div className="cardTitle">{list.pelicula_titulo}</div>
+        <div className="cardSynopsis">{list.pelicula_sinopsis}</div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="container">{array}</div>
+  );
 };
